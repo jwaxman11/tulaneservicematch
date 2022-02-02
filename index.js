6a661c0298b5919cd67dddd679a906fcf4ccaeaf
jwaxman11 <69725308+jwaxman11@users.noreply.github.com>
2022-02-02 12:25:32 -0600

    Update index.js
    function sendDataToServer(survey) {
        //send Ajax request to your web server.
        alert("The results are:" + JSON.stringify(survey.data));
    }

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 Survey.StylesManager.applyTheme("modern");
 
-var surveyJSON = {"title":"Service Match Survey","description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1},{"type":"ranking","name":"question2","title":"Rank the following hobbies","isRequired":true,"choices":[{"value":"1","text":"Gardening"},{"value":"item2","text":"Painting"},{"value":"item3","text":"Tutoring"},{"value":"item4","text":"Building"}],"choicesOrder":"asc"}]}]}
+var surveyJSON = {
+                "title":"Service Match Survey",
+                "description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You",
+                "pages":[{"name":"page1",
+                "elements":[{"type":"checkbox",
+                "name":"question1",
+                "title":"Do you require remote volunteering?","isRequired":true,
+                "choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],
+                "maxSelectedChoices":1},{"type":"ranking","name":"question2",
+                "title":"Rank the following hobbies",
+                "isRequired":true,
+                "choices":[{"value":"1","text":"Gardening"},{"value":"item2","text":"Painting"},{"value":"item3","text":"Tutoring"},{"value":"item4","text":"Building"}],"choicesOrder":"asc"}]}]}
 
-function sendDataToServer(survey) {
-    //send Ajax request to your web server.
-    alert("The results are:" + JSON.stringify(survey.data));
-}
+
+const survey = new Survey.Model(surveyJson);
+
+
 
 var survey = new Survey.Model(surveyJSON);
 $("#surveyContainer").Survey({
