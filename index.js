7711b7f2d4c9749bb7eb8176cb02ae9a741bb75f
jwaxman11 <69725308+jwaxman11@users.noreply.github.com>
2022-02-02 12:13:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-Survey
-    .StylesManager
-    .applyTheme("modern");
+Survey.StylesManager.applyTheme("modern");
 
-const surveyJson = {
-    elements: [{
-        name: "FirstName",
-        title: "Enter your first name:",
-        type: "text"
-    }, {
-        name: "LastName",
-        title: "Enter your last name:",
-        type: "text"
-    }]
-};
+var surveyJSON = {"title":"Service Match Survey","description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1},{"type":"ranking","name":"question2","title":"Rank the following hobbies","isRequired":true,"choices":[{"value":"1","text":"Gardening"},{"value":"item2","text":"Painting"},{"value":"item3","text":"Tutoring"},{"value":"item4","text":"Building"}],"choicesOrder":"asc"}]}]}
 
-const survey = new Survey.Model(surveyJson);
-survey.focusFirstQuestionAutomatic = false;
-
-function alertResults (sender) {
-    const results = JSON.stringify(sender.data);
-    alert(results);
+function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
 }
 
-survey.onComplete.add(alertResults);
-
-$(function() {
-    $("#surveyContainer").Survey({ model: survey });
+var survey = new Survey.Model(surveyJSON);
+$("#surveyContainer").Survey({
+    model: survey,
+    onComplete: sendDataToServer
 });
