a040dce47db9eb767d34c5419b7b74a6b7a4636a
jwaxman11 <69725308+jwaxman11@users.noreply.github.com>
2022-02-02 12:04:20 -0600

    Update survey.js

--- survey.js
+++ survey.js
@@ -1,1 +1,29 @@
+Survey
+    .StylesManager
+    .applyTheme("modern");
 
+const surveyJson = {
+    elements: [{
+        name: "FirstName",
+        title: "Enter your first name:",
+        type: "text"
+    }, {
+        name: "LastName",
+        title: "Enter your last name:",
+        type: "text"
+    }]
+};
+
+const survey = new Survey.Model(surveyJson);
+survey.focusFirstQuestionAutomatic = false;
+
+function alertResults (sender) {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+}
+
+survey.onComplete.add(alertResults);
+
+$(function() {
+    $("#surveyContainer").Survey({ model: survey });
+});
