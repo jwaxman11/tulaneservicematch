b26da2f9e75b15dce0bec31eab07b0ffc3509d26
jwaxman11 <69725308+jwaxman11@users.noreply.github.com>
2022-02-02 19:28:26 +0000

    updated

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ Survey
     .StylesManager
     .applyTheme("modern");
 
-const surveyJson = {{"title":"Service Match Survey","description":"DAnswering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1},{"type":"checkbox","name":"question3","title":"Do you have access to a car?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}]},{"type":"ranking","name":"question2","title":"Rank the following by your level of interest","isRequired":true,"choices":[{"value":"item1","text":"Gardening"},{"value":"item2","text":"Painting"},{"value":"item3","text":"Tutoring"},{"value":"item4","text":"Construction work"}]},{"type":"checkbox","name":"question4","title":"Are you comfortable working with children?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}]}]}]}
+const surveyJson = {"title":"Service Match Survey","description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1},{"type":"checkbox","name":"question3","title":"Do you have access to a car?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}]},{"type":"ranking","name":"question2","title":"Rank the following by your level of interest","isRequired":true,"choices":[{"value":"item1","text":"Gardening"},{"value":"item2","text":"Painting"},{"value":"item3","text":"Tutoring"},{"value":"item4","text":"Construction work"}]},{"type":"checkbox","name":"question4","title":"Are you comfortable working with children?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}]}]}]}
+
 
 
     
@@ -14,8 +15,16 @@ function alertResults (sender) {
     alert(results);
 }
 
+function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
+}
+
 survey.onComplete.add(alertResults);
 
 $(function() {
-    $("#surveyContainer").Survey({ model: survey });
+    $("#surveyContainer").Survey({ 
+			model: survey,
+			onComplete: sendDataToServer 
+			})
 });
