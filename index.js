3747bb3a0341d2732273617bf709cfc3118aaf29
jwaxman11 <69725308+jwaxman11@users.noreply.github.com>
2022-02-02 18:54:05 +0000

    p

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Survey
     .StylesManager
     .applyTheme("modern");
 
-const surveyJson = {"title":"Service Match Survey","description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1}]}]}
+const surveyJson = {"title":"Tulane Service Match Survey","description":"Answering These Questions Will Help Us Recommend The Most Suitable Volunteering Service For You","pages":[{"name":"page1","elements":[{"type":"checkbox","name":"question1","title":"Do you require remote volunteering?","isRequired":true,"choices":[{"value":"item1","text":"Yes"},{"value":"item2","text":"No"}],"maxSelectedChoices":1}]}]}
 
     
 
